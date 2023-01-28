20fbdd00e01c59f3b329e6eb359052a348f1503b
chenZhuoYue <1819439692@qq.com>
2023-01-28 15:48:36 +0800

    watch 属性

--- src/App.vue
+++ src/App.vue
@@ -15,26 +15,208 @@
   <!-- <RouterView /> -->
 
   <div>{{ Man }}</div>
-  <br>
+  <div>{{ Man2 }}</div>
   <button @click="change">修改</button>
+  <br>
+  -------------
+  <form>
+    <input v-model="form.name" type="text">
+    <br>
+    <input v-model="form.age" type="number">
+    <br>
+    <button @click.prevent="submit">提交</button>
+  </form>
+  <br>
+  -------------
+  <div>{{ testToRef }}</div>
+  <div>toRef:{{ name }}</div>
+  <button @click="changeSecond">修改</button>
+  <br>
+  -------------
+  <table>
+    <thead>
+      <tr>
+        <th>名称</th>
+        <th>数量</th>
+        <th>单价</th>
+        <th>操作</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr :key="index" v-for="(item, index) in tool">
+        <td align="center">{{ item.name }}</td>
+        <td align="center">
+          <button @click="computedNum(item, true)">+</button>
+          {{ item.num }}
+          <button @click="computedNum(item, false)">-</button>
+        </td>
+        <td align="center">{{ item.price }}</td>
+        <td align="center"><button @click="del(index)">删除</button></td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>总价：{{ $total }}</td>
+    </tfoot>
+  </table>
+  -------------
+  <form>
+    <input v-model="msg.foo.bar.name" type="text">
+    <br>
+    <input v-model="msg.foo.bar.age" type="number">
+    <br>
+  </form>
 </template>
 
 <script setup lang="ts">
 // import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
 
-import {ref} from 'vue'
-import type {Ref} from 'vue'
+import {ref, isRef, shallowRef, triggerRef, reactive, shallowReactive, readonly, toRef, toRefs, toRaw, computed, watch, watchEffect} from 'vue'
 
-type M = {
-  name: string
-}
-const Man:Ref<M> = ref({name: '小满'})
+/* 
+ * ref, isRef, shallowRef, triggerRef
+ -----------------------------------
+ * 所有被ref包裹的对象才是响应式的
+ * isRef() 可以判断一个对象是不是响应式
+ * shallowRef 为浅层响应式，只能影响到value层级，无法影响value的值
+ * ! ref和shallowRef不能一起使用，否则ref会将整个对象置为响应式，shallowRef将失去浅层响应的含义
+ * triggerRef() 用于响应被shallowRef()包裹的值
+*/
+const Man = ref({name: '小满'})
+const Man2 = shallowRef({name: '满2'})
 
 const change = () => {
   Man.value.name = '打满'
-  console.log(Man);
+  Man2.value.name = '本来是无法被影响的，但是加了triggerRef后变成响应式'
+  triggerRef(Man2)
+  console.log(Man2);
+  console.log(isRef(Man2));
 }
+
+/*
+ * reactive, shallowReactive, readonly
+ ---------------------------------------
+ * reactive与ref不同，只支持引用类型（array, object, map, set）, ref支持所有类型
+ * ref的取值都需要加上.value前缀，reactive不需要前缀可以直接把值点出来
+ * reactive 是一种proxy不能直接赋值，否则会破坏响应式对象
+ * 数组可以使用push和解构进行赋值
+ * shallowReactive是引用类型的浅层响应（只会对第一层有作用）
+ * readonly() 把对象转为只读，但是会受到原始对象的影响。只读对象指向的原始对象修改了，只读对象也会修改
+*/
+
+let form = reactive({
+  name: 'Anna',
+  age: 23
+})
+
+const submit = () => {
+  console.log(form, 'form-----');
+  
+}
+
+/* 
+ * toRef toRefs toRaw
+ ----------------------------
+ * toRef可以从对象中结构一个值出来
+ * toRef只能影响响应式对象，对非响应式对象无效
+ * toRefs() 用于把对象中的每一个值都变成ref类型
+ * ! 当使用reactive操作时，使用toRef结构对象，才能时结构的值为响应式，否则无法进行视图更新
+ * toRaw用于把响应式*对象*转为普通对象
+ */
+
+const testToRef = reactive({
+  name: 'Anna',
+  age: 23
+})
+
+const name = toRef(testToRef, 'name') // "Anna"
+
+const { age } = toRefs(testToRef)
+
+const changeSecond = () => {
+  name.value = 'Lili'
+  console.log(name, 'name-----');
+  console.log(age, 'age-------');
+  console.log(toRaw(testToRef), 'toRaw-----');
+  
+}
+
+
+/*
+ * computed 计算属性
+ -----------------------
+*/
+
+type Shop = {
+  name: string,
+  num: number,
+  price: number
+}
+let $total = ref(0)
+const tool = reactive<Shop[]>([
+  {
+    name: '衣服',
+    num: 1,
+    price: 10
+  },
+  {
+    name: '裤衩',
+    num: 2,
+    price: 20
+  },
+  {
+    name: '鞋子',
+    num: 3,
+    price: 30
+  }
+])
+const computedNum = (item:Shop, bool:Boolean):void => {
+  if(item.num < 99 && bool) {
+    item.num ++
+  }
+  if(item.num > 0 && !bool) {
+    item.num --
+  }
+}
+const del = (index: number):void => {
+  tool.splice(index, 1)
+}
+
+$total = computed<number>(() => {
+  return tool.reduce((prev, next) => {
+    return prev + (next.num * next.price)
+  }, 0)
+})
+
+/*
+ * watch
+ ---------------
+ * 只有响应式数据才能被watch监听到
+ * watch( [监听源], (newVal, oldVal) => {}, { option })
+ * 监听深度嵌套对象中的一个值时，需要使用函数return来保持其响应式
+ * reactive会默认开启深监听, ref不会
+*/
+
+let msg = reactive({
+  foo: {
+    bar: {
+      name: 'Anna',
+      age: 18
+    }
+  }
+})
+
+watch(() => msg.foo.bar.name, (newVal, oldVal) => {
+  console.log(newVal, oldVal);
+}, {
+  // deep: true ,
+  // immediate: true
+  // flush: 'pre' // 执行顺序，pre：组件更新前调用；sync：同步执行；post：组件更新后执行
+})
+
 
 </script>
 

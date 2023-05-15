2bc2dbe58297abbfbca3c520763def05f6bb7474
chenZhuoYue <1819439692@qq.com>
2023-05-15 19:40:18 +0800

    使用指令实现图片懒加载

--- src/views/custom-directives/index.vue
+++ src/views/custom-directives/index.vue
@@ -20,12 +20,16 @@
 
   图片懒加载
   <hr>
+  <!-- <img v-lazy alt=""> -->
+  <img alt="" v-for="(item, index) in pic_list" :key="index" v-lazy="item" style="height: 400px; width: 300px;">
 
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
 import type { Directive, DirectiveBinding} from 'vue'
+
+
 
 // 按钮的展示和消失
 const authority = ['BUT_A', 'BUT_B', 'BUT_C']
@@ -49,6 +53,30 @@ const vMove:Directive<HTMLElement, string> = (el:HTMLElement, bingding:Directive
   }
   moveElement.addEventListener('mousedown', mouseDown)
 }
+
+// 图片懒加载
+/**
+ * glob: 默认为懒加载引入，后面加一个{ eager: true }参数后为静态的模块引入
+ */
+const pic_list_obj = import.meta.glob('@/assets/test_pic/*.*', { eager: true })
+let pic_list = Object.values(pic_list_obj).map((v:any) => v.default)
+const def = '/src/assets/logo.svg'
+
+let vLazy:Directive<HTMLImageElement, string> = async(el, bingding) => {
+  el.src = def
+  const observer = new IntersectionObserver((entries) => {
+    if(entries[0].intersectionRatio > 0) {
+      setTimeout(() => {
+        el.src = bingding.value
+        observer.unobserve(el)
+      }, 1000)
+    }
+  })
+  observer.observe(el)
+}
+
+console.log(pic_list, 'pic_list-------')
+
 </script>
 
 <style lang="scss" scoped>
